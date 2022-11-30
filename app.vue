33bf20597c27d91d58c123e768538f726f23df34
nguyentamthanh <thanhh1602k@gmail.com>
2022-12-01 05:18:28 +0700

    init

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    hello
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+<script setup>
+useHead({
+  title: "Tic tac toe",
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/nuxt.png",
+    },
+  ],
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+const player = ref("X");
+const board = ref([
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]);
+const CalculateWinner = (board) => {
+  console.log("🚀 ~ file: index.vue:9 ~ CalculateWinner ~ board", board);
+};
+CalculateWinner(board.value);
+</script>
+<template>
+  <div class="bg-red-500">hello</div>
+</template>
